63d29d86a6ec87de55736bac2d6276df6fbcb614
GhosttBatt <ghosttbatt@gmail.com>
2025-08-19 18:08:16 +0530

    play.py

--- Clonify/utils/inline/play.py
+++ Clonify/utils/inline/play.py
@@ -63,10 +63,18 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+            InlineKeyboardButton(
+                text="ɴᴇᴛᴡᴏʀᴋ", url=f"https://t.me/HeartBeat_Offi"
+            ),
+            InlineKeyboardButton(
+                text="ᴄʜᴀᴛ", url=f"https://t.me/HeartBeat_Fam"
+            ),
+        ],
     ]
     return buttons
 
@@ -88,6 +96,7 @@ def stream_markup(_, chat_id):
                 text="ᴄʜᴀᴛ", url=f"https://t.me/HeartBeat_Fam"
             ),
         ]
+    ]
     return buttons
 
 
