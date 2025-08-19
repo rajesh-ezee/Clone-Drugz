5beebda101b737fd28be2e57b19a2813313ff36a
GhosttBatt <ghosttbatt@gmail.com>
2025-08-19 13:07:08 +0530

    ping.py

--- Clonify/cplugin/ping.py
+++ Clonify/cplugin/ping.py
@@ -5,7 +5,7 @@ import psutil
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from config import SUPPORT_CHAT, PING_IMG_URL
+from config import SUPPORT_CHAT, PING_IMG_URL, CHAT, SUPPORT_CHANNEL
 from .utils import StartTime
 from Clonify.utils import get_readable_time
 from Clonify.utils.database.clonedb import get_owner_id_from_db, get_cloned_support_chat, get_cloned_support_channel
@@ -45,7 +45,9 @@ async def ping_clone(client: Client, message: Message):
 ๏ **ᴅɪsᴋ :** {disk}""",
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=C_SUPPORT_CHAT)],
+                [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=CHAT)],
+                [InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url=SUPPORT_CHANNEL)],
+                
             ]
         ),
     )
